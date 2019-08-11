9ab6f28a597762e7d88ea576e6703609197ce8ec
白鳥雅人 <whitesplash0703@icloud.com>
2019-08-11 19:06:40 +0900

    Lesson7課題
    キューブに効果音を付けて、キューブと地面に接触した場合効果音を鳴らすようにしました。

---
+++ Assets/CubeController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour {
+    private float speed = -0.2f;
+    private float deadLine = -10;
+
+    AudioSource audioSource;
+	// Use this for initialization
+	void Start () {
+        audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(this.speed, 0, 0);
+        if(transform.position.x < this.deadLine)
+        {
+            Destroy(gameObject);
+        }
+	}
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "CubeTag" || collision.gameObject.tag == "GroundTag")
+        {
+            audioSource.Play();
+        }
+    }
+}
---
+++ Assets/block.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class block : MonoBehaviour {
+    public AudioClip sound1;
+    AudioSource audioSource;
+	// Use this for initialization
+	void Start () {
+        audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision.gameObject.tag);
+        if (collision.gameObject.tag == "CubeTag" || collision.gameObject.tag == "GroundTag")
+        {
+            Debug.Log("playSE");
+            audioSource.PlayOneShot(sound1);
+        }
+    }
+}
